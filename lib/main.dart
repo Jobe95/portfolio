9641beb6ab3b2b0d8940b01979646cbe80f31d0b
jobe95 <jonatanbengtsson95@hotmail.se>
2020-03-25 14:22:58 +0100

    Prepp for deploy

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:portfolio_jobe/pages/home_page.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
-      title: 'Jonatan-Portfolio',
+void main() => runApp(App());
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Jonatan Portfolio',
       theme: ThemeData(
         primaryColor: Color(0xFFC6D8D3),
         accentColor: Color(0xFFFDF0D5),
       ),
       home: HomePage(),
-    ),
-  );
+    );
+  }
 }
